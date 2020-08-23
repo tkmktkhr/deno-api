76b4a97027de8a4dd28d1d1e4695b105be5fced3
tkmktkhr <mukku.wordpress@gmail.com>
2020-08-23 14:06:12 +0900

    Implement findAll method, router

--- src/infrastructures/router.ts
+++ src/infrastructures/router.ts
@@ -1,3 +1,17 @@
 import { Router } from "../deps.ts";
+import { Vocabulary } from "../entities/vocabulary.ts";
+import { VocabularyController } from "../interfaces/controllers/vocabulary/vocabularyController.ts";
 
 export const router = new Router();
+const vocabularyController = new VocabularyController();
+
+router.get(
+  "/vocabularies",
+  async (context) => {
+    // controller
+    const result = await vocabularyController.findAllVocabularies();
+    console.log("Ended process here is Router");
+    console.log(result);
+    context.response.body = result;
+  },
+);
